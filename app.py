59da6d244c4683534139740a32877dc9f7c3cc03
Jorrit Bootsma <jorrit@Jorrits-MacBook-Pro.local>
2024-06-13 20:22:04 +0200

    added creation of csv file with all standings

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if uploaded_files:
         filename = uploaded_file.name
         euro_data = pd.read_excel(uploaded_file)
 
-        # Display the DataFrame
+        # Display the Input Excel as DataFrame
         # st.write(f"DataFrame from the uploaded file: {uploaded_file.name}")
         # st.dataframe(euro_data)
 
@@ -32,20 +32,36 @@ if uploaded_files:
         row_values = [filename] + row_values
 
         if first:
-            # Create the DataFrame
+            # Create the Match Scores DataFrame
             result_df = pd.DataFrame([row_values], columns=column_names)
+            # Create the Standings DataFrame
+            euro_data.iloc[1, 0] = filename
+            standings_df = euro_data.iloc[1:5, [0, 1, 4, 7, 10, 13, 16]]
+            empty_row = pd.DataFrame([[None] * len(standings_df.columns)], columns=standings_df.columns)
+            standings_df = pd.concat([standings_df, empty_row], ignore_index=True)
+            # Keep track of having processed the first file
             first = False
         else:
-            # Append the DataFrame
+            # Append the Match Scores DataFrame
             result_df = pd.concat([result_df, pd.DataFrame([row_values], columns=column_names)], ignore_index=True)
-            # result_df = result_df.append(pd.DataFrame([row_values], columns=column_names))
+            # Append the Standings DataFrame
+            euro_data.iloc[1, 0] = filename
+            empty_row = pd.DataFrame([[None] * len(standings_df.columns)], columns=standings_df.columns)
+            standings_df = pd.concat([standings_df, euro_data.iloc[1:5, [0, 1, 4, 7, 10, 13, 16]], empty_row], ignore_index=True)
 
-    # Display the DataFrame
-    st.write("DataFrame from the uploaded files:")
+    standings_df.columns = ["Filename", "Group A", "Group B", "Group C", "Group D", "Group E", "Group F"]
+
+    # Display the Match Scores DataFrame
+    st.write("CSV file containing scores of individual matches:")
     st.dataframe(result_df)
+    # Display the Standings DataFrame
+    st.write("CSV file containing standings after Poule Stage:")
+    st.dataframe(standings_df)
 
     # Doesn't work in the deployed app:
     # st.button(
     #     "Save to Excel",
     #     on_click=lambda: result_df.to_excel("euro_data.xlsx", index=False)
     # )
+
+

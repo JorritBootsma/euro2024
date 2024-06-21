04371454a5b72acfb6ae6d7057aef7f05550331b
Jorrit Bootsma <jorrit@Jorrits-MBP.home>
2024-06-21 14:40:53 +0200

    fix filenotfound error in analyse_knockout

--- pages/2_Analyse_Knock-outfase.py
+++ pages/2_Analyse_Knock-outfase.py
@@ -15,18 +15,21 @@ dict_for_sorting = {
     "R2": "d",
 }
 
-df = pd.read_csv("data/resultaten_knockoutfase.csv")
-match_cols = get_match_cols(df)
-match_cols.sort(key=lambda x: dict_for_sorting[x.split()[0]])
-df["Naam"] = df["filename"].map(lambda x: Path(x).stem)
-name_column = "Naam"
+try:
+    df = pd.read_csv("data/resultaten_knockoutfase.csv")
+    match_cols = get_match_cols(df)
+    match_cols.sort(key=lambda x: dict_for_sorting[x.split()[0]])
+    df["Naam"] = df["filename"].map(lambda x: Path(x).stem)
+    name_column = "Naam"
 
-# Dropdown menu for match selection
-match = st.selectbox('Select a match:', match_cols)
-first_in_standings = st.selectbox('Highlight de score van:', df[name_column].dropna().tolist(), index=None)
-score_of_first_in_standings = None
-if first_in_standings:
-    score_of_first_in_standings = df[df[name_column] == first_in_standings][match].values[0]
+    # Dropdown menu for match selection
+    match = st.selectbox('Select a match:', match_cols)
+    first_in_standings = st.selectbox('Highlight de score van:', df[name_column].dropna().tolist(), index=None)
+    score_of_first_in_standings = None
+    if first_in_standings:
+        score_of_first_in_standings = df[df[name_column] == first_in_standings][match].values[0]
 
-fig = bar_chart(df, match, names_column_name=name_column, x_axis_label="Score", score_of_first_in_standings=score_of_first_in_standings)
-st.plotly_chart(fig)
+    fig = bar_chart(df, match, names_column_name=name_column, x_axis_label="Score", score_of_first_in_standings=score_of_first_in_standings)
+    st.plotly_chart(fig)
+except FileNotFoundError:
+    st.error("Er zijn nog geen voorspellingen voor de knock-outfase geupload.")
